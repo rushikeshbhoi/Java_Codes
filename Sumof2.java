ae52b3e86b177b5dd8d38e24990e9626d66701a5
Rushikesh Bhoi <73032807+rushikeshbhoi@users.noreply.github.com>
2022-10-13 00:34:39 +0530

    Add files via upload

---
+++ Sumof2.java
@@ -0,0 +1,10 @@
+public class Sumof2 {
+    public static void main(String args[]){
+        int a;
+        Scanner sc = new Scanner(System.in);
+        var = sc(next.in);
+       // int sum = a + b;
+        System.out.println(var);
+
+    }
+}
