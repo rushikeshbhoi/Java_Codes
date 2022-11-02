41681e5a322226056e2cf88381bbb810dfc35c26
Rushikesh Bhoi <73032807+rushikeshbhoi@users.noreply.github.com>
2022-11-02 23:18:53 +0530

    Add files via upload

---
+++ StringInput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class StringInput {
+    public static void main(String args[]) {
+        System.out.println("Enter your Full Name ");
+        Scanner sc = new Scanner(System.in);
+        String print_name = sc.nextLine();
+
+        System.out.println("Your full name is : " + print_name);
+
+    }
+}
