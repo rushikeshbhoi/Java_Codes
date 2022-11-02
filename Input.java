29e25329108a26aa26c092314903f5db37cb6f0c
Rushikesh Bhoi <73032807+rushikeshbhoi@users.noreply.github.com>
2022-11-02 23:09:59 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Input {
+    public static void main(String args[]) {
+
+        System.out.println("Enter a number which you want to be printed");
+        Scanner sc = new Scanner(System.in);
+        String output = sc.next();
+
+        System.out.println("The output number is " + output);
+
+    }
+}
